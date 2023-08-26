39268d0b607b19c7f1b8a62aa162b3b61cdc4643
Hammad Abbas <141501366+Hammadabbasoffc@users.noreply.github.com>
2023-08-26 10:43:21 +0500

    Add files via upload

---
+++ Java Script/Js_Statements.js
@@ -0,0 +1,5 @@
+let x , y, z; // So this is statement 1
+x = 5; // this statement 2
+y = 6; // this is statement 3
+console.log(x+y);
+// So these are the statements line by line 
---
+++ Java Script/Syntax.js
@@ -0,0 +1,27 @@
+// how to create variables 
+
+var x;
+var y;
+
+// how to use variables
+// first you have to declear the value to the variable then use it 
+
+x = 40;
+y = 34;
+let z = x+y;
+
+console.log(z);
+// now want to check the value of x and y seprately 
+
+console.log(x);
+// value of y 
+console.log(y);
+
+// ************* Java Script Operators **********
+// JavaScript uses arithmetic operators ( + - * / ) to compute values:
+
+// want to multiply x with y then
+console.log(x*y);
+// want to divide x with y then
+console.log(x/y);
+
